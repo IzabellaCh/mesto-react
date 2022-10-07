ded4bed7899fcfc98b779a97f71364110065c477
Izabella Chebakova <chebakovaizabella@gmail.com>
2022-10-07 16:50:55 +0400

    Добавление интерактивности кнопки Сохранить после нажатия на нее

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useClose from '../utils/useClose';
 
 function PopupWithForm({name, title, isOpen, onClose, children, onSubmit}) {
+  const [saveButton, setSaveButton] = useState('Сохранить');
+
+  function handleClick() {
+    setSaveButton('Сохранение...');
+    setTimeout(setSaveButton, 1000, 'Сохранить');
+  }
+
   useClose(isOpen, onClose);
 
     return (
@@ -11,7 +18,7 @@ function PopupWithForm({name, title, isOpen, onClose, children, onSubmit}) {
         <h2 className="popup__heading">{title}</h2>
         <form className={`popup__form popup__form_type_${name}`} onSubmit={onSubmit} noValidate>
           {children}
-          <button type="submit" className="popup__save-button">Сохранить</button> 
+          <button type="submit" onClick={handleClick} className="popup__save-button">{saveButton}</button> 
         </form>
         <button className="popup__close-button button" onClick={onClose}></button>
       </div>
