2e19f4456a4aed7bf242d0875706ca0602a777fe
Izabella Chebakova <chebakovaizabella@gmail.com>
2022-10-07 19:48:57 +0400

    Удаление App.test.js

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -12,8 +12,7 @@ function PopupWithForm({name, title, isOpen, onClose, children, onSubmit}) {
   useClose(isOpen, onClose);
 
     return (
-    <div 
-      className={`popup popup_type_${name} ${isOpen ? "popup_opened" : ""}`}>
+    <div className={`popup popup_type_${name} ${isOpen ? "popup_opened" : ""}`}>
       <div className={`popup__container popup__container_type_${name}`}>
         <h2 className="popup__heading">{title}</h2>
         <form className={`popup__form popup__form_type_${name}`} onSubmit={onSubmit} noValidate>
